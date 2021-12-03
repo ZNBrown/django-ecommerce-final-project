d6555cf3b120445df4bc75660a36321a60282f47
ZNBrown <ZachNatBrown@gmail.com>
2021-12-03 11:36:09 +0000

    initial commit with models and boilerplate for django

---
+++ community_store_app/communities/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Community, Product
+
+admin.site.register(Community)
+admin.site.register(Product)
+
+
+# Register your models here.
+
---
+++ community_store_app/communities/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Community(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    sold_status = models.BooleanField()
+    image = models.ImageField()
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    community_id = models.ForeignKey(Community, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+    
+
