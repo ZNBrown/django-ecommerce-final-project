e2deb4326def1f0eae6472c09fe1ab0900782d5b
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-12-08 12:17:53 +0000

    Store image in db when form is submitted

--- community_store_app/community_store_app/urls.py
+++ community_store_app/community_store_app/urls.py
@@ -21,7 +21,6 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('communities/', include('communities.urls')),
